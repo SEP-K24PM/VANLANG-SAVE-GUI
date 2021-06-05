aa57d24b4fe9ef1eb9bf4328bf593e842030269e
nqnduy <nqn.duy@gmail.com>
2021-06-05 11:36:36 +0700

    Sign In Page

--- dest/main.js
+++ dest/main.js
@@ -236,3 +236,16 @@ $(btnCloseDP).on("click", function () {
     $(".popupTrade .blacklayer").removeClass("open");
     $(".popupTrade .inventory").removeClass("open");
 });
+window.addEventListener("load", function () {
+    const loginForm = document.querySelector(".signin-form");
+    const showPasswordIcon =
+        loginForm && loginForm.querySelector(".show-password");
+    const inputPassword =
+        loginForm && loginForm.querySelector('input[type="password"');
+    showPasswordIcon.addEventListener("click", function () {
+        const inputPasswordType = inputPassword.getAttribute("type");
+        inputPasswordType === "password"
+            ? inputPassword.setAttribute("type", "text")
+            : inputPassword.setAttribute("type", "password");
+    });
+});
