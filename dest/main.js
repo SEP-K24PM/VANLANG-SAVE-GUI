ac598b05c73c331d87675daf07dc55032a66017d
nqnduy <nqn.duy@gmail.com>
2021-06-06 23:46:31 +0700

    Inventory-page, update search market, table productDetail

--- dest/main.js
+++ dest/main.js
@@ -6,6 +6,7 @@ function toggleNavigation() {
     $("header .mainHeader .blacklayer").toggleClass("open");
     $("header .mainHeader .label").toggleClass("open");
     $("header .mainHeader #sidebar .navigation").toggleClass("open");
+    $("header .mainHeader #sidebar .signin-check").toggleClass("open");
 }
 $(logo).on("click", function () {
     toggleNavigation();
@@ -14,37 +15,10 @@ $(logo).on("click", function () {
 /**
  * Data Product
  */
-
-let suggestions = [
-    "Channel",
-    "CodingLab",
-    "CodingNepal",
-    "YouTube",
-    "YouTuber",
-    "YouTube Channel",
-    "Blogger",
-    "Bollywood",
-    "Vlogger",
-    "Vechiles",
-    "Facebook",
-    "Freelancer",
-    "Facebook Page",
-    "Designer",
-    "Developer",
-    "Web Designer",
-    "Web Developer",
-    "Login Form in HTML & CSS",
-    "How to learn HTML & CSS",
-    "How to learn JavaScript",
-    "How to became Freelancer",
-    "How to became Web Designer",
-    "How to start Gaming Channel",
-    "How to start YouTube Channel",
-    "What does HTML stands for?",
-    "What does CSS stands for?",
-];
-
+let datasugg = ["NhatDuy", "Dep trai"];
+console.log(typeof datasugg);
 /**
+ *
  * Data Product
  */
 $(document).ready(function () {
@@ -57,6 +31,10 @@ $(document).ready(function () {
                 "rgb(249, 244, 242"
             );
             $("header .mainHeader #sidebar nav ul li a").css(
+                "color",
+                "#333333"
+            );
+            $("header .mainHeader #sidebar .signin-check").css(
                 "color",
                 "#333333"
             );
@@ -74,6 +52,13 @@ $(document).ready(function () {
             );
         });
     } else if ($(".market-page").length != 0) {
+        let suggestions = [];
+        let a = document.querySelectorAll(
+            ".mainProduct .mainProduct__list .mainProduct__list-item .tittle"
+        );
+        a.forEach((element) => {
+            suggestions.push(element.textContent);
+        });
         const searchWrapper = document.querySelector(".search-input");
         const inputBox = searchWrapper.querySelector("input");
         const suggBox = searchWrapper.querySelector(".autocom-box");
@@ -86,12 +71,6 @@ $(document).ready(function () {
             let userData = e.target.value; //user enetered data
             let emptyArray = [];
             if (userData) {
-                icon.onclick = () => {
-                    webLink = "https://www.google.com/search?q=" + userData;
-                    linkTag.setAttribute("href", webLink);
-                    console.log(webLink);
-                    linkTag.click();
-                };
                 emptyArray = suggestions.filter((data) => {
                     //filtering array value and user characters to lowercase and return only those words which are start with user enetered chars
                     return data
@@ -147,6 +126,64 @@ $(document).ready(function () {
                 )
                 .classList.add("disable");
         }
+        let btnCheck = $(".button-check");
+        /**Inventory */
+        $(btnCheck).on("click", function () {
+            $(btnCheck).removeClass("fillColor");
+            $(btnCheck).children().removeClass("scaleNormal");
+            $(this).toggleClass("fillColor");
+            $(this).children().toggleClass("scaleNormal");
+        });
+
+        let btnDel = $(".button-control .del");
+        $(btnDel).on("click", function () {
+            $(btnCheck).removeClass("fillColor");
+            $(btnCheck).children().removeClass("scaleNormal");
+        });
+        let btnOpenDP = $(
+            ".detailproduct-page .detailProduct .container .detail__left .showUp"
+        );
+        $(btnOpenDP).on("click", function () {
+            $(".popupTrade .blacklayer").addClass("open");
+            $(".popupTrade .inventory").addClass("open");
+        });
+        let btnCloseDP = $(".popupTrade .inventory__close");
+        $(btnCloseDP).on("click", function () {
+            $(".popupTrade .blacklayer").removeClass("open");
+            $(".popupTrade .inventory").removeClass("open");
+        });
+    } else if ($(".signin-page").length != 0) {
+        const loginForm = document.querySelector(".signin-form");
+        const showPasswordIcon =
+            loginForm && loginForm.querySelector(".show-password");
+        const inputPassword =
+            loginForm && loginForm.querySelector('input[type="password"');
+        showPasswordIcon.addEventListener("click", function () {
+            const inputPasswordType = inputPassword.getAttribute("type");
+            inputPasswordType === "password"
+                ? inputPassword.setAttribute("type", "text")
+                : inputPassword.setAttribute("type", "password");
+        });
+    } else if ($(".inventory-page").length != 0) {
+        $(".inventory .searchWrap .search-input input").on(
+            "keyup",
+            function () {
+                var value = $(this).val().toLowerCase();
+                $(
+                    ".tableIven .inventory .inventory__wrap-bottom table tbody tr"
+                ).filter(function () {
+                    $(this).toggle(
+                        $(this).text().toLowerCase().indexOf(value) > -1
+                    );
+                });
+            }
+        );
+        let btnCheck = $(".button-check");
+        /**Inventory */
+        $(btnCheck).on("click", function () {
+            $(this).toggleClass("fillColor");
+            $(this).children().toggleClass("scaleNormal");
+        });
     }
 });
 
@@ -209,43 +246,4 @@ $(
     ".detailproduct-page .anotherProduct .container .product__button .btn.next"
 ).on("click", function () {
     $anotherProduct.flickity("next");
-});
-let btnCheck = $(".button-check");
-
-$(btnCheck).on("click", function () {
-    $(btnCheck).removeClass("fillColor");
-    $(btnCheck).children().removeClass("scaleNormal");
-    $(this).toggleClass("fillColor");
-    $(this).children().toggleClass("scaleNormal");
-});
-
-let btnDel = $(".button-control .del");
-$(btnDel).on("click", function () {
-    $(btnCheck).removeClass("fillColor");
-    $(btnCheck).children().removeClass("scaleNormal");
-});
-let btnOpenDP = $(
-    ".detailproduct-page .detailProduct .container .detail__left .showUp"
-);
-$(btnOpenDP).on("click", function () {
-    $(".popupTrade .blacklayer").addClass("open");
-    $(".popupTrade .inventory").addClass("open");
-});
-let btnCloseDP = $(".popupTrade .inventory__close");
-$(btnCloseDP).on("click", function () {
-    $(".popupTrade .blacklayer").removeClass("open");
-    $(".popupTrade .inventory").removeClass("open");
-});
-window.addEventListener("load", function () {
-    const loginForm = document.querySelector(".signin-form");
-    const showPasswordIcon =
-        loginForm && loginForm.querySelector(".show-password");
-    const inputPassword =
-        loginForm && loginForm.querySelector('input[type="password"');
-    showPasswordIcon.addEventListener("click", function () {
-        const inputPasswordType = inputPassword.getAttribute("type");
-        inputPasswordType === "password"
-            ? inputPassword.setAttribute("type", "text")
-            : inputPassword.setAttribute("type", "password");
-    });
 });
